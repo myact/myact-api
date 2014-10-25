b13fd9718b27de6319eff23fb05c44e6b9189302
Andrew Duthie <aduthie7@gmail.com>
2014-10-24 22:14:41 -0400

    Resolve JSON formatting error

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ var secrets = require( './config.secret' );
 
 module.exports = {
     db: {
-        client: secrets.db.client
+        client: secrets.db.client,
         connection: secrets.db.connection
     },
     passwordHashFactor: 10
