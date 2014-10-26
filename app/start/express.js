06b55bf00f772afd8c812f5f1eed00650a3b765f
Andrew Duthie <aduthie7@gmail.com>
2014-10-26 15:54:49 -0400

    Add method-override middleware

--- app/start/express.js
+++ app/start/express.js
@@ -2,6 +2,7 @@ module.exports.upstream = function( app ) {
     app.use( require( 'compression' )() );
     app.use( require( 'cors' )() );
     app.use( require( 'body-parser' ).json() );
+    app.use( require( 'method-override' )() );
 };
 
 module.exports.downstream = function( app ) {
