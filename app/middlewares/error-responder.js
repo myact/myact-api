4b467aff8f1d4cef05f821b8f5157960630846be
Andrew Duthie <aduthie7@gmail.com>
2014-10-26 11:18:22 -0400

    Move downstream error responder to own middleware

---
+++ app/middlewares/error-responder.js
@@ -0,0 +1,20 @@
+var BaseError = require( '../errors/base' ),
+    UnknownError = require( '../errors/unknown' ),
+    InvalidRequestError = require( '../errors/invalid-request' );
+
+module.exports = function( error, req, res, next ) {
+    if ( error instanceof SyntaxError ) {
+        error = new InvalidRequestError();
+    }
+
+    if ( ! ( error instanceof BaseError ) ) {
+        console.error( error.stack );
+        error = new UnknownError();
+    }
+
+    res.status( error.code ).send({
+        code: error.code,
+        type: error.type,
+        message: error.message
+    });
+};
--- app/start/express.js
+++ app/start/express.js
@@ -1,7 +1,3 @@
-var BaseError = require( '../errors/base' ),
-    UnknownError = require( '../errors/unknown' ),
-    InvalidRequestError = require( '../errors/invalid-request' );
-
 module.exports.upstream = function( app ) {
     app.use( require( 'compression' )() );
     app.use( require( 'cors' )() );
@@ -9,20 +5,5 @@ module.exports.upstream = function( app ) {
 };
 
 module.exports.downstream = function( app ) {
-    app.use(function( error, req, res, next ) {
-        if ( error instanceof SyntaxError ) {
-            error = new InvalidRequestError();
-        }
-
-        if ( ! ( error instanceof BaseError ) ) {
-            console.error( error.stack );
-            error = new UnknownError();
-        }
-
-        res.status( error.code ).send({
-            code: error.code,
-            type: error.type,
-            message: error.message
-        });
-    });
+    app.use( require( '../middlewares/error-responder' ) );
 }
