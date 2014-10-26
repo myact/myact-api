9f0ca21695d38662360cea61ed03236bf3613a50
Andrew Duthie <aduthie7@gmail.com>
2014-10-26 16:38:41 -0400

    Update retrieve-token helper to use superagent

--- test/db/helpers/retrieve-token.js
+++ test/db/helpers/retrieve-token.js
@@ -1,12 +1,12 @@
 var Promise = require( 'bluebird' ),
-    request = Promise.promisifyAll( require( 'supertest' ) );
+    request = Promise.promisifyAll( require( 'superagent' ) );
 
-module.exports = function() {
-    return request( this.app )
-        .post( '/login' )
+module.exports = function( root, done ) {
+    return request
+        .post( root + '/login' )
         .send({ email: 'admin@example.com', password: 'not-so-secret' })
         .endAsync()
         .then(function( res ) {
-            return res.body.token;
+            done( null, res.body.token );
         });
 };
