284adb2f2e0868ed42a2d5f6ff1948a49d1a97f6
Andrew Duthie <aduthie7@gmail.com>
2014-10-29 20:40:25 -0400

    Use raw knex for db seeding

--- app/db/seeds/settings.js
+++ app/db/seeds/settings.js
@@ -1,9 +1,5 @@
-require( '../../start/db' )();
-
-var Setting = require( '../../models/setting' );
-
 exports.seed = function( knex, Promise ) {
-    return new Setting().save({
+    return knex( 'settings' ).insert({
         name: 'defaultInterval',
         value: 60000
     });
