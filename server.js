84ca23b5686049e0451e5f0448843eb260d413d9
Andrew Duthie <aduthie7@gmail.com>
2014-12-01 20:09:01 -0500

    Correctly retrieve environment port value

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Myact = require( './app' ),
     http = require( 'http' );
 
 new Myact().start().then(function( app ) {
-    var port = process.env.NODE_ENV || 3000;
+    var port = process.env.PORT || 3000;
     http.createServer( app ).listen( port );
     console.log( 'Listening on port %d', port );
 });
